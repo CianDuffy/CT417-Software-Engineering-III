6b0fab55cbd8324e57033a6cf546dd8b779d13f4
Cian Duffy <cianduffyty@gmail.com>
2016-09-27 10:36:42 +0100

    Added CourseProgram Class and .jar module

--- Assignment 1 - Student Registration System/src/test/java/com/CianDuffy/CT417/StudentTests.java
+++ Assignment 1 - Student Registration System/src/test/java/com/CianDuffy/CT417/StudentTests.java
@@ -1,12 +1,10 @@
 package com.CianDuffy.CT417;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.joda.time.DateTime;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 /**
  * Unit tests for Student Class.
@@ -14,12 +12,12 @@ import java.util.Date;
 public class StudentTests extends TestCase {
 
     private Student testStudent;
-    private Date DOB;
+    private DateTime DOB;
 
     protected void setUp() {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
         try {
-            DOB = simpleDateFormat.parse("01/01/1994");
+            DOB = new DateTime(simpleDateFormat.parse("01/01/1994"));
             testStudent = new Student("Nick Faldo", DOB, "1234567");
         } catch(ParseException parseException) {
             parseException.printStackTrace();
